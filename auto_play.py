48f6a5e1e1a632491f6d920470c279ca29c803cb
BennyWang1007 <bennywang1007@gmail.com>
2024-08-04 02:47:31 +0800

    Delete Obsolete Files

--- auto_play.py
+++ auto_play.py
@@ -2,75 +2,20 @@ import random
 import sys
 import time
 
+import cv2
 import numpy as np
 from ppadb.device import Device as AdbDevice
 
-from constant import *
-from MoveDir import MoveDir
-from read_board import read_board
-from route_planning import *
-from route_planning_c import route_planning_c
-from Runes import Runes
-from TosGame import TosGame
-from utils import *
+from util.read_board import read_board
+from routing.route_planning import *
+from routing.route_planning_c import route_planning_c
+from tosgame.MoveDir import MoveDir
+from tosgame.Runes import Runes, Rune
+from tosgame.TosGame import TosGame
+from util.constant import *
+from util.utils import *
 
-
-"""-----------------send event constant and functions-----------------"""
-
-# suitable for Asus Zenfone M2
-EV_SYN = 0
-EV_KEY = 1
-EV_ABS = 3
-
-SYN_REPORT = 0
-ABS_MT_POSITION_X = 53
-ABS_MT_POSITION_Y = 54
-ABS_MT_TRACKING_ID = 57
-BTN_TOUCH = 330
-
-DEV = '/dev/input/event1'
-
-# for ld player
-ABS_MT_TRACKING_ID = 58
-DEV = '/dev/input/event2'
-
-def sendevent(device: AdbDevice,type: int, code: int, value: int, dev: str=DEV):
-    device.shell(f'sendevent {dev} {type} {code} {value}')
-
-def send_SYN_REPORT(device: AdbDevice, dev: str=DEV) -> None:
-    sendevent(device, EV_SYN, SYN_REPORT, 0, dev)
-
-def send_BTN_TOUCH_DOWN(device: AdbDevice, dev: str=DEV) -> None:
-    sendevent(device, EV_KEY, BTN_TOUCH, 1, dev)
-
-def send_POSITION(device: AdbDevice, x: int, y: int, dev: str=DEV) -> None:
-    sendevent(device, EV_ABS, ABS_MT_POSITION_X, x, dev)
-    sendevent(device, EV_ABS, ABS_MT_POSITION_Y, y, dev)
-    send_SYN_REPORT(device, dev)
-
-def send_ABS_MT_TRACKING_ID(device: AdbDevice, x: int, dev: str=DEV) -> None:
-    sendevent(device, EV_ABS, ABS_MT_TRACKING_ID, x, dev)
-
-
-"""-----------------send event functions-----------------"""
-
-def route_move(device: AdbDevice, route: list[tuple[int, int]]) -> None:
-    route_loc = [get_grid_loc(x, y) for x, y in route]
-    tolerance = RUNE_SIZE // 4
-    dx, dy = random.randint(-tolerance, tolerance), random.randint(-tolerance, tolerance)
-    route_loc = [(x + RUNE_SIZE // 2 + dx, y + RUNE_SIZE // 2 + dy) for x, y in route_loc]
-    route_loc = [(y, SCREEN_WIDTH - x) for x, y in route_loc] # weird coordinate system of ld player
-
-    send_ABS_MT_TRACKING_ID(device, 1)
-    sendevent(device, EV_KEY, BTN_TOUCH, 1)
-
-    for x, y in route_loc:
-        send_POSITION(device, x, y)
-
-    send_ABS_MT_TRACKING_ID(device, -1)
-    sendevent(device, EV_KEY, BTN_TOUCH, 0)
-    send_SYN_REPORT(device)
-
+from util.events import route_move
 
 if __name__ == "__main__":
 
@@ -91,7 +36,7 @@ if __name__ == "__main__":
     score = 0
     final_route = []
 
-    cd_image = cv2.imread('image/cd.png', IMREAD_MODE)
+    cd_image = cv2.imread('templates/cd.png', IMREAD_MODE)
     h, w = cd_image.shape[:2]
     h, w = int(h * SCREEN_WIDTH / 1080.), int(w * SCREEN_HEIGHT / 1920.)
     cd_image = cv2.resize(cd_image, (w, h), interpolation=cv2.INTER_AREA)
@@ -111,7 +56,7 @@ if __name__ == "__main__":
 
             # if cd is found, then read board
             if len(loc[0]) > 0:
-                game = read_board(device)
+                game = read_board(device, read_effect=False)
                 indices = get_indices_from_route()
                 if all([game.board[indices[i, j]].rune != Runes.UNKNOWN.value for i in range(5) for j in range(6)]):
                     break
@@ -135,7 +80,7 @@ if __name__ == "__main__":
             # score, final_route = route_planning(game, iter, max_first_depth, max_depth, True)
             final_route = route_planning_c(game, iter, max_first_depth, max_depth, False)
             
-        game_before_move = read_board(device)
+        game_before_move = read_board(device, read_effect=False)
         if (game_before_move == game):
             indices = get_indices_from_route(final_route)
             print_two_board(game.board, None, indices)
